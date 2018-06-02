e9385d2659976639982e79720c3a676ff496d765
AliMasarweh <33619630+AliMasarweh@users.noreply.github.com>
2018-06-02 18:05:29 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,3 +1,104 @@
+#include "Board.h"
+
+
+Board::Board() {
+	this->sizeOfBoard = 4;
+	this->board = new Status*[this->sizeOfBoard];
+	int i;
+	for (i = 0; i < this->sizeOfBoard; i++)
+		this->board[i] = new Status[this->sizeOfBoard];
+}
+
+Board::Board(int size) {
+	this->sizeOfBoard = size;
+	this->board = new Status*[this->sizeOfBoard];
+	int i;
+	for (i = 0; i < this->sizeOfBoard; i++)
+		this->board[i] = new Status[this->sizeOfBoard];
+}
+
+Board::Board(const Board& newBoard) {
+	this->sizeOfBoard = newBoard.sizeOfBoard;
+	this->board = new Status*[this->sizeOfBoard];
+	int i, j;
+	for (i = 0; i < this->sizeOfBoard; i++) {
+		this->board[i] = new Status[this->sizeOfBoard];
+		for (j = 0; j < this->sizeOfBoard; j++)
+			this->board[i][j] = newBoard.board[i][j];
+	}
+}
+
+Board::~Board() {
+	int i;
+	for (i = 0; i< this->sizeOfBoard; i++)
+		delete[] board[i];
+	delete[] board;
+}
+
+Board& Board::operator=(const Board& newBoard) {
+	this->~Board();
+	this->sizeOfBoard = newBoard.sizeOfBoard;
+	board = new Status*[this->sizeOfBoard];
+	int i, j;
+	for (i = 0; i < this->sizeOfBoard; i++) {
+		board[i] = new Status[this->sizeOfBoard];
+		for (j = 0; j < this->sizeOfBoard; j++)
+			this->board[i][j] = newBoard.board[i][j];
+	}
+	return *this;
+}
+
+Board& Board::operator= (const char newStatus) {
+	Status status(newStatus);
+	int i, j;
+	for (i = 0; i < this->sizeOfBoard; i++)
+		for (j = 0; j < this->sizeOfBoard; j++)
+			this->board[i][j] = status;
+	return *this;
+}
+
+Status& Board::operator[] (Coordination const &Coordination) const {
+	if (Coordination.getXCoordination() < this->sizeOfBoard && Coordination.getXCoordination() >= 0 &&
+		Coordination.getYCoordination() < this->sizeOfBoard && Coordination.getYCoordination() >= 0)
+		return board[Coordination.getXCoordination()][Coordination.getYCoordination()];
+	throw IllegalCoordinateException(Coordination);//Invalied Coordination
+}
+
+ostream& operator<< (ostream& os, const Board& inserteBoard) {
+	int i, j;
+	for (i = 0; i < inserteBoard.sizeOfBoard; i++) {
+		for (j = 0; j < inserteBoard.sizeOfBoard; j++)
+			os << inserteBoard.board[i][j];
+		os << endl;
+	}
+	return os;
+}
+/*Insert a string or a file represting a board*/
+istream& operator>> (istream &is, Board &board) {
+	string line;
+	is >> line;
+	int size = line.length();
+	Board temp(size);
+	int i;
+	for (i = 0; i<size; i++) temp[{0, i}] = line[i];
+	int num = 1;
+	while (num < size && is >> line) {
+		i = 0;
+		for (i = 0; i<size; i++) temp[{num, i}] = line[i];
+		num++;
+	}
+	board = temp;
+	return is;
+}
+
+unsigned int Board::size() const {
+	return this->sizeOfBoard;
+}
+int Board::BoardSize() const {
+	return this->sizeOfBoard;
+}
+
+
 string Board::draw(int num) {
 	Board board{ sizeOfBoard };
 	board = *this;
@@ -212,4 +313,9 @@ string Board::draw(int num) {
 	output.write(reinterpret_cast <char*>(&image), 3 * num*num);
 	output.close();
 	return fileName;
+}
+
+bool Board::exists(const std::string& name) {
+	ifstream f(name.c_str());
+	return f.good();
 }
